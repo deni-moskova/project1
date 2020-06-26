9de6c2f6040b455fd6a4c10442bc52cdba5292bb
deni-moskova <65388058+deni-moskova@users.noreply.github.com>
2020-06-26 22:40:34 +0300

    minor updates

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+;(function($, window, document, undefined) {
+
+	$(document).ready(function(){
+		console.log(1);
+
+		// let value=$('.menu');
+
+		// value.on('click', function(event), {
+
+		// })
+
+
+	});
+		
+
+})(jQuery, window, document);
